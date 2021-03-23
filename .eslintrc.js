3710a1b951b74b5f7319426e7ec4440a1db64bbe
TheComputerM <mudit.harshit99@outlook.com>
2021-03-23 09:34:39 +0530

    improve linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'svelte3'],
   env: {
     es6: true,
     browser: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'airbnb-base'],
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:import/typescript'],
   overrides: [
     {
       files: ['**/tests/**/*.ts'],
@@ -14,5 +14,16 @@ module.exports = {
         jest: true,
       },
     },
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
   ],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+  },
+  settings: {
+    'svelte3/ignore-styles': () => true,
+  },
 };
