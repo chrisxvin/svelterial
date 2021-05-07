9999c3fcef0c0a12d4a4f7f345f059f1bb868f59
TheComputerM <mudit.harshit99@outlook.com>
2021-05-07 16:12:15 +0530

    update index.js

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,1 +1,7 @@
 export { default as MaterialApp } from './MaterialApp.svelte';
+export { default as Button } from './Button.svelte';
+export { default as Icon } from './Icon.svelte';
+export { default as Divider } from './Divider.svelte';
+export { default as Avatar } from './Avatar.svelte';
+export { default as Overlay } from './Overlay.svelte';
+export { default as Alert } from './Alert.svelte';
