0e12161ed4ee451a24aa4e6610658cbdec34b782
TheComputerM <mudit.harshit99@outlook.com>
2021-05-07 16:07:08 +0530

    add lists

---
+++ packages/list/List.svelte
@@ -0,0 +1,32 @@
+<script>
+  let klass = '';
+  export { klass as class };
+</script>
+
+<div class="s-list {klass}">
+  {#if $$slots.title}
+    <span class="s-list__title">
+      <slot name="title" />
+    </span>
+  {/if}
+  <slot />
+</div>
+
+<style svelterial>
+  .s-list {
+    margin: 0;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    position: relative;
+  }
+
+  .s-list__title {
+    display: block;
+    padding: 8px 16px;
+    text-transform: uppercase;
+    font-size: 0.8rem;
+    font-weight: 600;
+    letter-spacing: 1px;
+    color: var(--s-text-secondary);
+  }
+</style>
---
+++ packages/list/ListItem.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Ripple from '@svelterialjs/core/utils/Ripple.js';
+  let klass = '';
+  export { klass as class };
+
+  export let dense = false;
+  export let nav = false;
+  export let selectable = false;
+  export let flat = false;
+  export let rounded = false;
+  export let ripple = selectable ? {} : null;
+  export let active = false;
+</script>
+
+<div
+  class="s-listitem {klass}"
+  class:s-listitem--dense={dense}
+  class:s-listitem--nav={nav}
+  class:s-listitem--selectable={selectable}
+  class:s-listitem--flat={flat}
+  class:s-listitem--rounded={rounded}
+  class:s-listitem--active={active}
+  use:Ripple={ripple}>
+  <slot name="prepend" />
+  <div class="s-listitem__content">
+    <div class="s-listitem__title">
+      <slot />
+    </div>
+    <div class="s-listitem__subtitle">
+      <slot name="subtitle" />
+    </div>
+  </div>
+  <slot name="append" />
+</div>
+
+<style svelterial="./styles/ListItem.scss"></style>
