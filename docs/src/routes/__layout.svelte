75125a3a30a3fcff0cdd60add5725c98d86c241e
TheComputerM <mudit.harshit99@outlook.com>
2021-05-09 17:21:37 +0530

    prism theme with css vars

--- docs/src/routes/__layout.svelte
+++ docs/src/routes/__layout.svelte
@@ -4,25 +4,25 @@
   import AppBar from '@svelterialjs/navigation/AppBar.svelte';
   import Button from '@svelterialjs/core/Button.svelte';
   import Icon from '@svelterialjs/core/Icon.svelte';
-  import { mdiGithub } from '@mdi/js';
+  import { mdiGithub, mdiWeatherSunny, mdiWeatherNight } from '@mdi/js';
   import links from '$lib/links';
   import { theme } from '$lib/stores';
-</script>
 
-<svelte:head>
-  {#if $theme === 'light'}
-    <link href="/prism-light.css" rel="stylesheet" />
-  {:else}
-    <link href="/prism-dark.css" rel="stylesheet" />
-  {/if}
-</svelte:head>
+  function toggleTheme() {
+    $theme = $theme === 'light' ? 'dark' : 'light';
+  }
+</script>
 
 <MaterialApp theme={$theme}>
   <AppBar position="fixed" class="inset-x-0">
     <h5 slot="title" class="ml-8px">Svelterial</h5>
     <div class="flex-grow" />
-    <Button variant="fab" size="small" flat href={links.github} target="_blank">
+    <Button text href="/docs">Docs</Button>
+    <Button variant="fab" size="small" text href={links.github} target="_blank">
       <Icon path={mdiGithub} />
+    </Button>
+    <Button variant="fab" size="small" text on:click={toggleTheme}>
+      <Icon path={$theme === 'light' ? mdiWeatherNight : mdiWeatherSunny} />
     </Button>
   </AppBar>
   <main class="pt-56px">
