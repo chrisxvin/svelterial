1ddc7f4a26206163ea2cfb6b4088ce3abc327e74
TheComputerM <mudit.harshit99@outlook.com>
2021-05-10 18:56:26 +0530

    add class prop in MaterialApp

--- packages/core/MaterialApp.svelte
+++ packages/core/MaterialApp.svelte
@@ -1,11 +1,18 @@
 <script>
   /**
+   * The classes added to the wrapping element.
+   */
+  let klass = '';
+  export { klass as class };
+
+  /**
    * The theme of the Svelterial material app.
    */
   export let theme = 'light';
+
 </script>
 
-<main class="s-app theme--{theme}">
+<main class="s-app theme--{theme} {klass}">
   <slot />
 </main>
 
