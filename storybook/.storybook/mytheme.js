82d56e33dba7363ce2759271523465cf70094fee
TheComputerM <mudit.harshit99@outlook.com>
2021-05-13 23:40:24 +0530

    add my theme

---
+++ storybook/.storybook/mytheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  colorSecondary: '#6200EA',
+  textColor: 'rgba(0,0,0,.87)',
+  brandTitle: 'Svelterial',
+});
