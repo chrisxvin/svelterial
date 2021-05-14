9bf23581052a71d00315d40463b7b13e7859c449
TheComputerM <mudit.harshit99@outlook.com>
2021-05-14 12:41:27 +0530

    add Icon story

---
+++ storybook/stories/Icon.stories.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import args from '../metadata';
+  import Icon from '@svelterialjs/core/Icon.svelte';
+  import { mdiHome, mdiGithub, mdiHuman, mdiFire } from '@mdi/js';
+</script>
+
+<Meta
+  title="Components/Core/Icon"
+  component={Icon}
+  argTypes={args('core/Icon', {
+    path: {
+      defaultValue: 'mdiHome',
+      options: ['mdiHome', 'mdiGithub', 'mdiHuman', 'mdiFire'],
+      mapping: { mdiHome, mdiGithub, mdiHuman, mdiFire },
+      control: {
+        type: 'select',
+      },
+    },
+    slot_default: {
+      table: {
+        disable: true,
+      },
+    },
+  })} />
+
+<Template let:args>
+  <Icon {...args} />
+</Template>
+
+<Story name="SVG" />
