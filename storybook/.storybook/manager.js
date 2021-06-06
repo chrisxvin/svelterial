4f4ed765a889f6365fa1be6b19f02adfdec08eba
TheComputerM <mudit.harshit99@outlook.com>
2021-06-06 21:57:38 +0530

    inline theme

--- storybook/.storybook/manager.js
+++ storybook/.storybook/manager.js
@@ -1,5 +1,12 @@
 import { addons } from '@storybook/addons';
-import mytheme from './mytheme';
+import { create } from '@storybook/theming';
+
+const mytheme = create({
+  base: 'light',
+  colorSecondary: '#651fff',
+  textColor: 'rgba(0,0,0,.87)',
+  brandTitle: 'Svelterial',
+});
 
 addons.setConfig({
   theme: mytheme,
--- storybook/.storybook/mytheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'light',
-  colorSecondary: '#651fff',
-  textColor: 'rgba(0,0,0,.87)',
-  brandTitle: 'Svelterial',
-});
