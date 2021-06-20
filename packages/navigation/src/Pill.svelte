99043008f4637bf6d8b007940446f96b269243f2
TheComputerM <mudit.harshit99@outlook.com>
2021-06-21 00:42:28 +0530

    add Pill

---
+++ packages/navigation/src/Pill.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let active = false;
+</script>
+
+<div class="s-pill" on:click class:s-pill--active={active}>
+  <slot />
+</div>
+
+<style svelterial="../styles/Pill.scss"></style>
